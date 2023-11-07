b324a28f8a95e28784258298d4ada31434c64144
Daniel Harding <danielharding2008@gmail.com>
2023-11-07 14:05:10 +0000

    Logic added to the question and answer selection, questions added in an object to the questions.js

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,7 +1,11 @@
 const startScreen = document.getElementById("start-screen");
 const questionsDiv = document.getElementById("questions");
+var questionTitle = document.getElementById("question-title");
 var timer = document.getElementById("time");
+var endScreen = document.getElementById("end-screen");
 var seconds = 60;
+var currentQuestionIndex = 0;
+var score = 0;
 
 // Get a reference to the "Start Quiz" button
 const startButton = document.getElementById("start");
@@ -9,11 +13,11 @@ const startButton = document.getElementById("start");
 // Add an event listener to the "Start Quiz" button
 // startButton.addEventListener;
 // "click", setTimer();
-startButton.addEventListener("click", startquiz);
+startButton.addEventListener("click", startQuiz);
 
 startButton.addEventListener("click", setTimer);
 
-function startquiz() {
+function startQuiz() {
   startScreen.style.display = "none";
   questionsDiv.style.display = "block";
 }
@@ -28,4 +32,34 @@ function setTimer() {
       timer.textContent = seconds;
     }
   }, 1000);
+}
+
+function updateQuestions() {
+  if (currentQuestionIndex < newQuestions.length) {
+    var currentQuestion = newQuestions[currentQuestionIndex];
+
+    questionTitle.textContent = currentQuestion.question;
+    questionsDiv.appendChild(questionTitle);
+
+    currentQuestion.answers.forEach((answers) => {
+      var questionbtn = document.createElement("button");
+      questionbtn.textContent = answers;
+      if (answers === currentQuestion.correctAnswer) {
+        questionbtn.className = "correct";
+      }
+      questionbutton.addEventListener("click", answerClick);
+      choicesContainer.appendChild(button);
+    });
+  } else {
+    endScreen.style.display = "show";
+  }
+}
+
+function answerClick(event) {
+  if (event.target.classList.contains("correct")) {
+    score++;
+  }
+
+  currentQuestionIndex++;
+  updateQuestions();
 }
---
+++ assets/js/questions.js
@@ -0,0 +1,30 @@
+var choicesContainer = document.querySelectorAll("choices");
+
+var newQuestions = [
+  {
+    question: "Which Company Developed Javascript?",
+    answers: ["Apple", "Mircosoft", "Dell", "Netscape"],
+    correctAnswer: "Netscape",
+  },
+  {
+    question: "Javascript is a _________ language",
+    answers: [
+      "Object-Oriented",
+      "Object-Based",
+      "Procedural",
+      "None of the Above",
+    ],
+    correctAnswer: "Object-Based",
+  },
+  {
+    question:
+      "Which of the following keywords in used to define a variable in Javascript?",
+    answers: ["var", "let", "Both A and B", "None of the Above"],
+    correctAnswer: "Both A and B",
+  },
+  {
+    question: "which of the following is not a Javascript Framework?",
+    answers: ["Node", "Vue", "React", "Cassandra"],
+    correctAnswer: "Cassandra",
+  },
+];
